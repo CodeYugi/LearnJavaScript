f434cbea54443287cdea02ce5aeb14a35a0036ab
Yugi <yugiicode@gmail.com>
2025-07-24 09:36:34 +0100

    added  more stuff

---
+++ Data type and oprations/NUMandMath.js
@@ -0,0 +1,60 @@
+// Data type and operations 
+
+// # Number And Math 
+// -> Don't take  stress about let and console.log by the that is for show output in console
+
+let a = 1; // giving value to a 
+let b = 2; // giving value to b
+// lets found addition , substraction ,  multiplication , division of a and b 
+let add = a + b; 
+let sub = a - b;
+let multi = a * b;
+let div =  b / a;
+console.log(add ,  sub , multi , div);
+
+// as you see  + for addition  , - for substraction , * for multiplication
+// and /  for division like this for remainder we use %
+// for assining a value we use = 
+
+// lets list them all 
+
+// (1)=> += 
+// if we need to add a value to  any variable oh it is simple by declaring other variable 
+
+let firstvar = 6;
+let finalvar = firstvar + 4;
+console.log(finalvar); // to check the output in console 
+// perfect but there is also another way to do this =>
+let secondvar = 6; 
+// now i want to add 4 to secondvar means 6 so 
+secondvar += 4;
+console.log(secondvar); // for same reason as before 
+// shortcut easy way or nice way to make it happen 
+
+// this is not only for addition 
+// multiplication *=
+// Division /=
+// substraction -=
+
+// two more ++ and --
+// ++ This is an even shorter shortcut specifically for increasing a variable's value by one
+// -- This  Decreases a variable's value by one
+
+// try by yourself once  and check out put on console using console.log
+
+// keep in mind they use BODMAS priroty
+
+// two types of number 
+let firsttype = 1; // integer (whole number)
+let secondtype = 2.3; // Floats 
+// float have minor inaccuracy because computer store value in 0 and 1
+
+// Math.round()
+let round = Math.round(2.876);
+
+console.log(round);
+
+
+
+
+
--- core concept/Coreconcept.js
+++ core concept/Coreconcept.js
@@ -1,3 +1,29 @@
 // console.log("hello world");
 
 // code => Code is the language used to communicate with computers.
+// running code => Compilation/Interpretation -> Loding -> Execution -> output 
+
+// Java script is  interpreted language 
+
+// what that means ? interpreted or compile 
+
+// Compilation => in this computer take your code and convert into machine lang 0 and 1
+//                then execute  your code........
+
+// interpreter => in this computer convert the code line by line to 0 and 1s and then execute
+//                rather than doing it once like compile lang
+
+//Fact
+// Compiled languages are faster because they translate the whole code into machine language
+// (0s and 1s) just once before running the program. This machine code runs directly 
+// on the computer’s hardware without extra work. On the other hand, 
+// interpreted languages read and execute the code line-by-line every time you run the program,
+// which takes extra time because the interpreter has to translate the code repeatedly.
+// So, compiling takes more time upfront but makes the program run faster, 
+// while interpreting is slower because it does the translation over and 
+// over during execution.
+
+// Syntax ?
+
+// what is syntax ? => Syntax refers to the rules that must be followed when writing code.
+//                     syntax is  Case Sensitive , semicolan placement....

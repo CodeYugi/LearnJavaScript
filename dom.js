a67161378ce1deadc03224f0971dbdef9416d32e
Yugi <yugiicode@gmail.com>
2025-08-06 05:26:11 +0100

    added new material

--- dom.js
+++ dom.js
@@ -21,6 +21,50 @@ console.log(thirdsibs.previousElementSibling);
 console.log(thirdsibs.nextElementSibling);
 
 
+let div = document.createElement("div");
+div.innerHTML = ("<p>HI I CREATED div FROM JAVA SCRIPT");
+  div.id = "phraaa";
+  div.firstChild.id = "first";
+  div.className = "classname";
+//   document.body.appendChild(div);
+
+console.log(div);
+
+
+
+let menu = document.getElementById("menu");
+
+let list = document.createElement("li");
+list.innerHTML = "LogOut";
+
+let list2 =  document.createElement("li")
+list2.innerHTML = "added"
+// menu.appendChild(list);
+// menu.append(list2, list);
+
+// menu.prepend(list2);
+
+menu.insertAdjacentHTML("beforeend" , "<li>before end</li>");
+menu.insertAdjacentHTML("beforebegin", "<li>before begin</li>");
+menu.insertAdjacentHTML("afterbegin" , "<li>afterbegin</li>");
+menu.insertAdjacentHTML("afterend" , "<li>afterend</li>");
+
+
+// let newnode = menu.cloneNode();
+// let newnode = menu.cloneNode(false);
+let newnode = menu.cloneNode(true);
+console.log(newnode);
+
+
+menu.removeChild(menu.lastElementChild);
+
+
+menu.insertBefore( newnode ,menu.firstElementChild)
+
+
+
+
+
 
 
 
